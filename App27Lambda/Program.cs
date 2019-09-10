f6952c8a1e0a54a40404f3c2498d0126704b1bbc
admin <admin@TA-8214-04>
2019-09-10 12:15:44 +0200

    opdateret

---
+++ App27Lambda/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace App27Lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> a = new List<int>() { 5, 1, 51, 7, 1, 56, 36, 5 };
+
+        }
+    }
+    public static bool Find (List<int> a)
+    {
+        foreach (var item in a)
+        {
+            if (item == 56) return true;
+        }
+    }
+}
